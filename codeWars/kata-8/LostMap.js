374ae2c3780605b6a2f698c93b907a32982dece8
gemtunde <gemtunde@gmail.com>
2022-08-13 12:46:28 +0100

    lost map

---
+++ codeWars/kata-8/LostMap.js
@@ -0,0 +1,21 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+// [1, 2, 3] --> [2, 4, 6]
+
+function maps(x){
+  let arr = [];
+  for(let i=0; i<x.length; i++){
+    arr.push(x[i]*2)
+  }
+  return arr 
+}
+
+console.log(maps([1, 2, 3]))//, [2, 4, 6]);
+    console.log(maps([4, 1, 1, 1, 4]))//, [8, 2, 2, 2, 8]); 
+    console.log(maps([2, 2, 2, 2, 2, 2]))//, [4, 4, 4, 4, 4, 4]); 
+
+//best practice
+function maps(x){
+  return x.map(n => n * 2);
+}
