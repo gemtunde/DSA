bfe0dc7e9bea47d5e33f360fb2b91b61e4db9a05
gemtunde <gemtunde@gmail.com>
2022-09-16 10:02:34 +0100

    segment arr algorithm

---
+++ random_algo/Segment_ObjectArr.js
@@ -0,0 +1,43 @@
+//question
+// let cars = [
+//   {
+//     model: 'Ford',
+//     color: 'Red',
+//   },
+//   {
+//     model: 'Skoda',
+//     color: 'Grey',
+//   },
+//   {
+//     model: 'Ford',
+//     color: 'Grey',
+//   }
+// ];
+
+//answer
+function transformData(datas){
+  
+   let model =[];
+   let color=[];
+    let arr = {
+     model:[],
+     color:[]
+    };
+ 
+ for(let i=0; i<datas.length; i++){
+    datas[i].model = model.push(datas[i].model)
+    datas[i].color = color.push(datas[i].color) 
+ }
+  arr.model.push([...new Set(model)]);
+  arr.color.push([...new Set(color)]);
+ return arr
+}
+
+
+ transformData(cars);
+
+// expected result
+// {
+//  models: ['Ford', 'Skoda'],
+//  colors: ['Red', 'Grey'],
+//};
