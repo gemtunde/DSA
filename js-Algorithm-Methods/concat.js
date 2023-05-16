b99de2c3a237dfe10f4da9d0d5549e81198c43dd
gemtunde <gemtunde@gmail.com>
2023-05-16 10:11:07 +0100

    concat  method

---
+++ js-Algorithm-Methods/concat.js
@@ -0,0 +1,12 @@
+
+//the original arrays are unchanged
+const a = [1,2,3];
+const b = [4,5,6];
+const c = [7,8,9] 
+
+//const d = a.concat(b);
+const e = a.concat(b, c);
+
+//console.log(d)
+console.log(e)
+
