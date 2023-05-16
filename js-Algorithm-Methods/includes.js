5181f69ba7914c6a43fc2f1e2bad6719e4e5ab64
gemtunde <gemtunde@gmail.com>
2023-05-16 12:25:24 +0100

    includes  method

---
+++ js-Algorithm-Methods/includes.js
@@ -0,0 +1,18 @@
+
+
+//includes return true if element is found in an array
+// or false if noelement is found
+
+const names = ["Tunde", "Ivan", "Ellen", "Shawn"];
+const res = names.includes("Tunde");
+
+if(res){
+    console.log("Yea... Am present");
+}else{
+    console.log("Sad....am Absent")
+}
+
+
+
+
+
