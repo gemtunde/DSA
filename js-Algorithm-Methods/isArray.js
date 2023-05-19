01fb6249c5bfae95cf590c4e78604294b8a91b4f
gemtunde <gemtunde@gmail.com>
2023-05-19 16:51:46 +0100

    isArray  method

---
+++ js-Algorithm-Methods/isArray.js
@@ -0,0 +1,9 @@
+
+//checks if a given value is an array
+// returns true of value is an array
+
+const numbers = [1,2,3,4,5]
+const str = "Am i an array"
+const number = 90 ;
+
+console.log(Array.isArray(numbers))
