459863baac3f626969640eea7e93811690998f96
gemtunde <gemtunde@gmail.com>
2023-05-21 06:11:33 +0100

    join  method

---
+++ js-Algorithm-Methods/join.js
@@ -0,0 +1,10 @@
+//creates new string from an array by concatenates all the element in an array
+// comma is the separator by default
+
+
+const africa = ['Mali', 'Sudan','Togo','Niger'];
+//const res = africa.join();
+//const res = africa.join(' - ');
+//const res = africa.join(' ');
+const res = africa.join(' - ');
+console.log(res);
