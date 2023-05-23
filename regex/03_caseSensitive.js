ea257e7aae928b0da493a80f5f2402dc68744d72
gemtunde <gemtunde@gmail.com>
2023-05-23 10:22:56 +0100

    case sensitive

---
+++ regex/03_caseSensitive.js
@@ -0,0 +1,9 @@
+
+
+// i flag is used to ignore case sentitivity
+
+let myClub = "my FootBall team is ChelsEA";
+let clubRegex = /Chelsea/i ;
+let result = clubRegex.test(myClub);
+console.log(result);
+
