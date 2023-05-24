154cc0f95fe7d4adf78602a311489da8d0ec15bb
gemtunde <gemtunde@gmail.com>
2023-05-24 13:31:03 +0100

    match Multiple Occurance

---
+++ regex/10_matchMultipleOccurance.js
@@ -0,0 +1,9 @@
+
+
+// + is used to match characters that occur one or more times
+//EXAMPLE 1
+let str = "hippoppotamus";
+let myRegex = /p+/g;
+let result = str.match(myRegex);
+console.log(result)
+
