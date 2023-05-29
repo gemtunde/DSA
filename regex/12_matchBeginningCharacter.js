c04fee8c52bc6a5f1dab47ada6282c8248b32ce2
gemtunde <gemtunde@gmail.com>
2023-05-29 11:41:34 +0100

    match Beginning Character

---
+++ regex/12_matchBeginningCharacter.js
@@ -0,0 +1,9 @@
+
+
+// ^ can also be used to test characters at the beginning of the string
+//returns true if CR7 is at the beginning of the string and false if not
+
+let str = "CR7 is a GOAT and plays excellently"
+let myRegex = /^CR7/ ;
+let result = myRegex.test(str);
+console.log(result)
