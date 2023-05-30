9e7688c42d74af8cb728c377139d840523be5663
gemtunde <gemtunde@gmail.com>
2023-05-30 14:55:41 +0100

    match Range Of Repeated Letters

---
+++ regex/19_mathRangeOfRepeatedLetters.js
@@ -0,0 +1,17 @@
+
+
+// match Range Of Repeated Letters
+// specify upper and lower number of matches
+//pattern matches
+//match multiple occerence of letters or numbers
+
+let str = "ohhh no";
+let myRegex = /oh{3,6} no/g;
+//let result = myRegex.test(str);
+let result = str.match(myRegex);
+console.log(result);
+
+
+
+
+
