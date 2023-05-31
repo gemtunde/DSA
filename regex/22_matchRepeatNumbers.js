00a17480ca74ea982e0854573d6253ed82a89091
gemtunde <gemtunde@gmail.com>
2023-05-31 15:08:15 +0100

    repeat numbers

---
+++ regex/22_matchRepeatNumbers.js
@@ -0,0 +1,12 @@
+
+//match repeated numbers or letters
+
+// let str = "regex regex";
+// let myRegex = /(\w+)\s\1/;
+//let result = myRegex.test(str);
+//let result = str.match(myRegex);
+
+// let number = "34 34 34";
+// let myRegex = /^(\d+)\s\1\s\1$/;
+
+// let result = myRegex.test(number);
