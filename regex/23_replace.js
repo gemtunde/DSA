0aca8726ecd50cafd2df6eae8f1e37b857139cc9
gemtunde <gemtunde@gmail.com>
2023-06-01 12:51:50 +0100

    replace empty space  in string

--- regex/23_replace.js
+++ regex/23_replace.js
@@ -12,9 +12,18 @@
 
 // EXAMPLE 2
 // SWITCH WORDS IN A STRING
-let str = "MESSI RONALDO";
-let myRegex = /(\w+)\s(\w+)/
-let result = str.replace(myRegex, '$2 $1') ; 
+// let str = "MESSI RONALDO";
+// let myRegex = /(\w+)\s(\w+)/
+// let result = str.replace(myRegex, '$2 $1') ; 
 
-console.log(result);
+// console.log(result);
+
+
+// EXAMPLE 3
+//REPLACE EMPTY SPACES IN STRING
+
+let str = "  i watch football  ";
+let myRegex = /^\s+|\s+$/g ;
+let result = str.replace(myRegex, "");
+console.log(result)
 
